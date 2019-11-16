2a4120f5cdcb025fc5802aa6e1f91a7a1db1d118
Hugo Erazo <hugogilmar@gmail.com>
2019-11-15 21:14:38 -0600

    👌 Code review fixes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -8,6 +8,6 @@ class ApplicationRecord < ActiveRecord::Base
 
   # Ensure model's uuid attribute is present
   def ensure_uuid
-    self.uuid = UUID.new.generate unless uuid.present?
+    self.uuid = UUID.new.generate if uuid.blank?
   end
 end

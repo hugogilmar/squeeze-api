af8e54969d5bfbafc39ca3a1945cc2990422e057
Hugo Erazo <hugogilmar@gmail.com>
2019-11-27 04:48:07 -0600

    👌 Code review fixes

--- app/mailers/squeeze/user_mailer.rb
+++ app/mailers/squeeze/user_mailer.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 module Squeeze
+  # User mailer
   class UserMailer < ApplicationMailer
+    # Email delivery test
     def email_delivery_check(user)
       mail(to: user.email, subject: 'Email delivery check')
     end

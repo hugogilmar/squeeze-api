80b96384e03a7de08b3bedb89eb5d3b028b91955
Hugo Erazo <hugogilmar@gmail.com>
2019-12-08 12:43:16 -0600

    ♻️ Persist commands refactors

--- app/commands/squeeze/v1/base/nested/persist_command.rb
+++ app/commands/squeeze/v1/base/nested/persist_command.rb
@@ -5,26 +5,24 @@ module Squeeze
     module Base
       module Nested
         # Persist nested command base class
-        class PersistCommand < MemberCommand
-          # Command execution
-          def call(params)
-            raise(ActiveRecord::RecordInvalid, form) unless form.validate(params)
-
-            form.sync
-            form.model.save!
-            success(serializer)
-          end
+        class PersistCommand < Base::PersistCommand
+          context current_user: nil, parent_resource_id: nil, resource_id: nil
 
           private
 
-          # Form builder
-          def form
-            @form ||= form_class.new(model)
+          # Model builder
+          def model
+            @model ||= parent_model.__send__(model_scope).find(resource_id)
           end
 
-          # Form class used for params validation
-          def form_class
-            raise(NotImplementedError, 'form_class method not implemented')
+          # Parent model builder
+          def parent_model
+            @parent_model ||= current_user.__send__(parent_scope).find(parent_resource_id)
+          end
+
+          # Parent scope
+          def parent_scope
+            raise(NotImplementedError, 'parent_scope method not implemented')
           end
         end
       end

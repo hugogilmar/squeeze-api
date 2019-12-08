15c4fefdc5634ab15cce4cdedbbc448087173309
Hugo Erazo <hugogilmar@gmail.com>
2019-12-08 06:44:30 -0600

    ✅ Code review fixes

--- app/models/squeeze/account.rb
+++ app/models/squeeze/account.rb
@@ -4,7 +4,7 @@ module Squeeze
   # Accounts model
   class Account < ApplicationRecord
     belongs_to :user
-    has_many :operations
+    has_many :operations, dependent: :destroy
 
     enum kind: %i[bank_account cash credit_card debit_card]
   end
